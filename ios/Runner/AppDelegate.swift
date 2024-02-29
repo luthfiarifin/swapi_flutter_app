ac23edd3a5d7a77835b629c48b1e76f14914048b
Luthfi Arifin <luthfiarifin0222@gmail.com>
2024-02-29 23:36:00 +0700

    feat: initial cinema page

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyASkT0Ogkq6BSbcJM5IrrUybO4mOiB7o8g")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/features/cinema/presentation/page/cinema_page.dart
+++ lib/features/cinema/presentation/page/cinema_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 @RoutePage()
 class CinemaPage extends StatefulWidget {
@@ -10,10 +14,61 @@ class CinemaPage extends StatefulWidget {
 }
 
 class _CinemaPageState extends State<CinemaPage> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  @override
+  void initState() {
+    _goToCurrentLocation();
+
+    super.initState();
+  }
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Cinema'),
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        myLocationButtonEnabled: false,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _goToCurrentLocation,
+        child: const Icon(Icons.gps_fixed),
+      ),
     );
+  }
+
+  Future<void> _goToCurrentLocation() async {
+    final GoogleMapController controller = await _controller.future;
+    final currentLocation = await _getUserCurrentLocation();
+
+    await controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(currentLocation.latitude, currentLocation.longitude),
+          zoom: 14.4746,
+        ),
+      ),
+    );
+  }
+
+  Future<Position> _getUserCurrentLocation() async {
+    await Geolocator.requestPermission().then((value) {}).onError(
+      (error, stackTrace) async {
+        await Geolocator.requestPermission();
+        debugPrintStack(stackTrace: stackTrace);
+      },
+    );
+
+    return await Geolocator.getCurrentPosition();
   }
 }
